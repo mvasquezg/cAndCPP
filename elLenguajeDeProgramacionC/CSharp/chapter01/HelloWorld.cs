215938e320fb640dbfa2e2540c3435f0cdd9fdc4
mvasquezg <ing.mvasquezg@gmail.com>
2022-04-26 23:26:28 -0500

    Programa 01.-Mensaje en pantalla Hola Mundo en lenguaje C#

---
+++ elLenguajeDeProgramacionC/CSharp/chapter01/HelloWorld.cs
@@ -0,0 +1,12 @@
+/*
+*Libro: The C Programming Language, Segunda Edicion.
+*Capitulo 01.
+*Programa 01: Mensaje en pantalla "Hola Mundo"
+*/
+using System;
+
+public class HelloWorld{
+    public static void Main(String[] args){ 
+        Console.WriteLine("Hola mundo en C#");
+    }//end main
+}//end Welcome
