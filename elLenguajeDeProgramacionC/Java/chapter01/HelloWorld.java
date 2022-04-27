7856442f79a30936b55846933e22cd67e17f7e90
mvasquezg <ing.mvasquezg@gmail.com>
2022-04-26 23:19:31 -0500

    Programa 01.-Mensaje en pantalla Hola Mundo en lenguaje Java

---
+++ elLenguajeDeProgramacionC/Java/chapter01/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+*Libro: The C Programming Language, Segunda Edicion.
+*Capitulo 01.
+*Programa 01: Mensaje en pantalla "Hola Mundo"
+*/
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hola Mundo\n");
+    }//end main
+}//end HelloWorld
