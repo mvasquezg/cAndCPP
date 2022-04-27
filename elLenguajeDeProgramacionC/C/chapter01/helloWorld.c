d2bf780fff48ebc03afbf842a4ed5c886cabaa65
mvasquezg <ing.mvasquezg@gmail.com>
2022-04-26 22:56:24 -0500

    Programa 01.-Mensaje en pantalla Hola Mundo en lenguaje c

---
+++ elLenguajeDeProgramacionC/C/chapter01/helloWorld.c
@@ -0,0 +1,9 @@
+/*
+*Libro: The C Programming Language, Segunda Edicion.
+*Capitulo 01.
+*Programa 01: Mensaje en pantalla "Hola Mundo"
+*/
+#include<stdio.h>
+void main(){
+    printf("Hola mundo\n");
+}//end main
