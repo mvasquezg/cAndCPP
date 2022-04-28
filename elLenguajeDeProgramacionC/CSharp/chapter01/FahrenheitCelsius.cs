e23ae75818d3fba97aa9ca8f17664fc308d1e095
mvasquezg <ing.mvasquezg@gmail.com>
2022-04-28 01:20:25 -0500

    Programa 02.-Calcula grados fahrenheit a grados centigrados en lenguaje C#

---
+++ elLenguajeDeProgramacionC/CSharp/chapter01/FahrenheitCelsius.cs
@@ -0,0 +1,25 @@
+/*
+*Libro: The C Programming Language, Segunda Edicion.
+*Capitulo 01.
+*Programa 02: Conversion de grados fahrenheit a grados centigrados
+*/
+class FahrenheitCelsius{
+    public static void Main(string[] args){
+        int fahrenheit, celsius;
+        int lower, upper, step;
+
+        lower=0;
+        upper=300;
+        step=20;
+
+        fahrenheit=lower;
+
+        System.Console.Write("Fahrenheit\t\tCelsius\n");
+        while(fahrenheit<=upper){
+            celsius=5*(fahrenheit-32)/9;
+            System.Console.Write(fahrenheit+"\t\t\t"+celsius+"\n");
+            fahrenheit=fahrenheit+step;
+        }
+
+    }///end main 
+}//FahrenheitCelsius
