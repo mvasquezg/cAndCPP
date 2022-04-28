f909f2e71a7f59900d36759bd67308a3d1e5c343
mvasquezg <ing.mvasquezg@gmail.com>
2022-04-28 01:14:08 -0500

    Programa 02.-Calcula grados fahrenheit a grados centigrados en lenguaje C++

---
+++ elLenguajeDeProgramacionC/CPP/chapter01/fahrenheitCelsius.cpp
@@ -0,0 +1,27 @@
+/*
+*Libro: The C Programming Language, Segunda Edicion.
+*Capitulo 01.
+*Programa 02: Conversion de grados fahrenheit a grados centigrados
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int fahrenheit, celsius;
+    int lower, upper, step;
+
+    lower=0;
+    upper=300;
+    step=20;
+
+    fahrenheit=lower;
+
+    cout<<"Fahrenheit\t\tCelsius\n";
+    while(fahrenheit<=upper){
+        celsius=5*(fahrenheit-32)/9;
+        cout<<fahrenheit<<"\t\t\t"<<celsius<<"\n";
+        fahrenheit=fahrenheit+step;
+    }
+
+    return 0;
+}
