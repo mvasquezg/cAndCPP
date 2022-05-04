a14f9f8cc26a43372277f91357cf5ed6a3cf3f56
mvasquezg <ing.mvasquezg@gmail.com>
2022-05-04 00:52:43 -0500

    Programa 02.-Calcula grados fahrenheit a grados centigrados en lenguaje Html, Css y Js

--- elLenguajeDeProgramacionC/htmlCssJS/js/cLenguageProgramming.js
+++ elLenguajeDeProgramacionC/htmlCssJS/js/cLenguageProgramming.js
@@ -1,4 +1,33 @@
-            
+var idTabDegrees=document.getElementById("idTabDegrees");           
+var idBtnClean=document.getElementById("idBtnClean");
+
+idBtnClean.style.display="none";
+
 function showMessage(){
     alert("Hola mundo");
+}
+
+function calculateFahtoCels(){
+        var fahrenheit, celsius;
+        var lower, upper, step;
+
+        lower=0;
+        upper=300;
+        step=20;
+
+        fahrenheit=lower;
+
+        idBtnClean.style.display="block";
+        idTabDegrees.innerHTML+="<tr><th>Fahrenheit</th><th>Celsius</th></tr>";
+        while(fahrenheit<=upper){
+            celsius=5*(fahrenheit-32)/9;
+            idTabDegrees.innerHTML+="<tr><td>"+fahrenheit.toFixed(2)+"</td><td>"+celsius.toFixed(2)+"</td></tr>";
+            fahrenheit=fahrenheit+step;
+        }
+}
+
+function cleanTable(){
+    idBtnClean.style.display="none";
+    idTabDegrees.innerHTML="";
+
 }
