9c5629231f745d9aa82b185d916a1c473ed34f27
mvasquezg <ing.mvasquezg@gmail.com>
2022-05-04 23:44:08 -0500

    Programa 02.-Calcula grados fahrenheit a grados centigrados con decimales usando ciclo for en lenguaje C#

--- elLenguajeDeProgramacionC/CSharp/chapter01/FahrenheitCelsius.cs
+++ elLenguajeDeProgramacionC/CSharp/chapter01/FahrenheitCelsius.cs
@@ -5,6 +5,8 @@
 */
 class FahrenheitCelsius{
     public static void Main(string[] args){
+        
+        /*Codigo con sentencia While
         float fahrenheit, celsius;
         float lower, upper, step;
 
@@ -19,6 +21,13 @@ class FahrenheitCelsius{
             celsius=5*(fahrenheit-32)/9;
             System.Console.Write(fahrenheit+"\t\t\t"+celsius+"\n");
             fahrenheit=fahrenheit+step;
+        }*/
+
+        int fahrenheit;
+   
+        System.Console.Write("Fahrenheit\t\tCelsius\n");
+        for(fahrenheit=0; fahrenheit<=300; fahrenheit+=20){
+            System.Console.Write(fahrenheit+"\t\t\t"+(5.0/9.0)*(fahrenheit-32)+"\n");
         }
 
     }///end main 
